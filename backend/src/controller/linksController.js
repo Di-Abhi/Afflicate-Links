235203f60cf7746fa291b8640860461da7d1bd48
Abhishek Kumar <abhi993575@gmail.com>
2025-07-09 11:49:47 +0530

    analytics

--- backend/src/controller/linksController.js
+++ backend/src/controller/linksController.js
@@ -1,4 +1,6 @@
+const Clicks = require("../model/Clicks");
 const Links = require("../model/Links");
+const axios = require('axios');
 
 const linksController={
     create:async(request,response)=>{
@@ -133,6 +135,35 @@ const linksController={
             if(!link){
                 return response.status(404).json({error:"Link does not exist with the given id"});
             }
+
+            const isDevelopment = process.env.NODE_ENV === 'development';
+            const ipAddress = isDevelopment
+                ? '8.8.8.8'
+                :request.headers['x-forwaded-for']?.split(',')[0] || request.socket.remoteAddress;
+            
+            const geoResponse = await axios.get(`http://ip-api.com/json/${ipAddress}`);
+            const {city, country, region, lat, lon, isp} = geoResponse;
+
+            const userAgent = request.headers['user-agent'] || 'Unknown';
+            const {deviceType, browser} = getDeviceInfo(userAgent);
+
+            const referrer = request.get('Referrer') || null;
+
+            await Clicks.create({
+                linkId: link._id,
+                ip: ipAddress,
+                city: city,
+                country: country,
+                region: region,
+                latitude: lat,
+                longitude: lon,
+                isp: isp,
+                referrer: referrer,
+                userAgent: userAgent,
+                deviceType: deviceType,
+                browser: browser,
+                clickedAt: new Date()
+            })
             
             link.clickCount+=1;
             await link.save();
@@ -141,6 +172,42 @@ const linksController={
         }catch(error){
             console.log(error);
             return response.status(500).json({error:"Internal server error"})
+        }
+    },
+    analytics: async (request,response)=>{
+        try{
+            const {linkId,from,to} = request.query;
+
+            const link = await Links.findById(linkId);
+            if(!link){
+                return response.status(404).json({
+                    error: 'Link not found'
+                });
+            }
+
+            const userId = request.user.role === 'admin'
+                ? request.user.id 
+                : request.user.adminId;
+            if(link.user.toString()!==userId){
+                return response.status(403).json({error: 'Unauthorized access'})
+            }
+
+            const query = {
+                linkId: linkId
+            };
+
+            if(from && to){
+                query.clickedAt= {$gte: new Date(from),$lte: new Date(to)};
+            }
+
+            const data= await Clicks.find(query).sort({clickedAt:-1});
+            response.json(data)
+        }
+        catch(error){
+            console.log(error);
+            response.status(500).json({
+                message: 'Interanl server error'
+            });
         }
     }
 };
